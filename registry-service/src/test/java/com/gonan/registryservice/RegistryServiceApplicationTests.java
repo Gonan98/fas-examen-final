cc127dd07e0016e29ba8083b390645ee34b9c9b7
Andre Gonzales <andreg-16@hotmail.com>
2021-07-05 00:11:54 -0500

    first commit

---
+++ registry-service/src/test/java/com/gonan/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonan.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
